5c4473c11e9ff9aa356b5415eb56a9e45b721e91
Loic L <l.laborderie-boulou@tbs-education.org>
2022-12-11 19:42:39 +0100

    animation logo finie, j'en ai chié mdr

---
+++ test.js
@@ -0,0 +1,35 @@
+const gallery1 = document.querySelector(".logo1");
+const gallery2 = document.querySelector(".logo2");
+const gallery3 = document.querySelector(".logo3");
+
+var imagesArray1 = Array.from(gallery1.children);
+var imagesArray2 = Array.from(gallery2.children);
+var imagesArray3 = Array.from(gallery3.children);
+
+function execute() {
+  const randomIndex1 = Math.floor(Math.random() * 9);
+  const randomIndex2 = Math.floor(Math.random() * 9);
+  const randomIndex3 = Math.floor(Math.random() * 9);
+  const target1 = imagesArray1[randomIndex1];
+  const target2 = imagesArray2[randomIndex2];
+  const target3 = imagesArray3[randomIndex3];
+  target1.classList.add("active");
+  setTimeout(() => {
+    target2.classList.add("active");
+  }, 100);
+  setTimeout(() => {
+    target3.classList.add("active");
+  }, 200);
+  console.log(target1 + "classlist added");
+  setTimeout(() => {
+    target1.classList.remove("active");
+    setTimeout(() => {
+      target2.classList.remove("active");
+    }, 100);
+    setTimeout(() => {
+      target3.classList.remove("active");
+    }, 200);
+  }, 2990);
+  setTimeout(execute, 3000);
+}
+execute();
